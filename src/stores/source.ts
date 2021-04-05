297a2f1922696ebc8bb5dcc5e49ad2423b0fd239
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-04-05 21:10:53 +0800

    feat: 修改文件源操作的逻辑

--- src/stores/source.ts
+++ src/stores/source.ts
@@ -7,12 +7,22 @@ const source = createSlice({
   name: 'source',
   initialState,
   reducers: {
+    /** 如果文件已存在，则替换原文件；否则在列表末尾添加新文件 */
     addSource(state, action: PayloadAction<SourceRecord>) {
-      state.push(action.payload);
+      const newFile = action.payload;
+      const idx = state.findIndex(file => file.filename === newFile.filename);
+      if (idx === -1) {
+        state.push(newFile);
+      } else {
+        state[idx] = newFile;
+      }
     },
+    /** 如果文件存在，则删除文件；否则什么都不做 */
     removeSource(state, action: PayloadAction<string>) {
       const idx = state.findIndex(record => record.filename === action.payload);
-      state.splice(idx, 1);
+      if (idx !== -1) {
+        state.splice(idx, 1);
+      }
     },
   },
 });

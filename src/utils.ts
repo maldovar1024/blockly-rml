672e44b969a246c149bab69203c0c7f659257945
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-04-08 22:38:11 +0800

    feat: 封装读取文件内容的功能

--- src/utils.ts
+++ src/utils.ts
@@ -14,6 +14,11 @@ export function downloadFile(content: string, filename: string) {
   document.body.removeChild(eleLink);
 }
 
+/**
+ * 上传文件
+ * @param accept 与 `input` 元素的 `accept` 属性含义相同
+ * @returns 返回用户选择的文件
+ */
 export function uploadFile(accept = ''): Promise<File | undefined> {
   const input = document.createElement('input');
   input.type = 'file';
@@ -28,4 +33,37 @@ export function uploadFile(accept = ''): Promise<File | undefined> {
   input.click();
   document.body.removeChild(input);
   return files;
+}
+
+/**
+ * 读取文本文件的内容
+ * @param file 要读取的文件
+ * @returns 返回读取到的字符串
+ */
+export async function readTextFile(file: File): Promise<string | undefined> {
+  const reader = new FileReader();
+  const readerPromise = new Promise<string | undefined>(resolve => {
+    reader.onload = () => {
+      const content = reader.result;
+      resolve(typeof content === 'string' ? content : undefined);
+    };
+  });
+  reader.readAsText(file);
+  return readerPromise;
+}
+
+/**
+ * 上传并读取文本文件的内容
+ * @param accept 与 `input` 元素的 `accept` 属性含义相同
+ * @returns 返回读取到的字符串
+ */
+export async function uploadAndReadTextFile(
+  accept = ''
+): Promise<string | undefined> {
+  const file = await uploadFile(accept);
+  if (file === undefined) {
+    return undefined;
+  }
+
+  return readTextFile(file);
 }

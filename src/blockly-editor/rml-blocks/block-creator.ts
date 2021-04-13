04043d785b5dd24043db2f2fdcce591e17be48e8
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-04-13 21:43:36 +0800

    feat: 添加创建块的工具函数

---
+++ src/blockly-editor/rml-blocks/block-creator.ts
@@ -0,0 +1,35 @@
+import { BlockSvg, WorkspaceSvg } from 'blockly';
+
+/** 块的描述 */
+export interface BlockShape {
+  /** 块的类型 */
+  type: string;
+  /** 块的所有 `field_input` 的名字和值 */
+  fields?: Record<string, unknown>;
+  /** 块的所有 `input_value` 的名字和值 */
+  inputs?: Record<string, BlockShape>;
+  /** 块的所有 `input_statement` 的名字和值 */
+  statements?: Record<string, BlockShape>;
+}
+
+export function createBlock(
+  workspace: WorkspaceSvg,
+  block: BlockShape
+): BlockSvg {
+  const { type, fields = {}, inputs = {}, statements = {} } = block;
+  const root = workspace.newBlock(type) as BlockSvg;
+  Object.keys(fields).forEach(name => {
+    root.setFieldValue(fields[name], name);
+  });
+  Object.keys(inputs).forEach(name => {
+    const input = createBlock(workspace, inputs[name]);
+    root.getInput(name).connection.connect(input.outputConnection);
+  });
+  Object.keys(statements).forEach(name => {
+    const statement = createBlock(workspace, statements[name]);
+    root.getInput(name).connection.connect(statement.previousConnection);
+  });
+  root.initSvg();
+  root.render();
+  return root;
+}

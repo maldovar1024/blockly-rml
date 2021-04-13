dbcdf8b1e221ddb0e562cd7a6bbfe788883e93b1
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-04-13 22:05:00 +0800

    feat: 点击CSV的列名创建相应的映射

--- src/blockly-editor/index.tsx
+++ src/blockly-editor/index.tsx
@@ -2,17 +2,52 @@ import BlocklyContainer from '@/blockly-container';
 import { WorkspaceChangeCallback } from '@/blockly-container/types';
 import { connect, setMappingCode } from '@/stores';
 import { BlocklyOptions, Events } from 'blockly';
-import { Component } from 'react';
+import { Component, createRef } from 'react';
 import { ConnectedProps } from 'react-redux';
 import initialWorkspace from './initial-workspace.xml';
 import customMenuOptions from './menu-options';
 import rmlBlocks from './rml-blocks';
+import { createBlock } from './rml-blocks/block-creator';
+import names from './rml-blocks/names';
 import RMLGenerator from './rml-generator';
 import toolbox from './toolbox.xml';
 
 const rmlGenerator = new RMLGenerator();
 
 class BlocklyEditor extends Component<BlocklyEditorProps> {
+  private ref = createRef<BlocklyContainer>();
+
+  componentDidUpdate(prevProps: BlocklyEditorProps) {
+    if (prevProps.newBlockParam.id !== this.props.newBlockParam.id) {
+      this.createMap();
+    }
+  }
+
+  private createMap() {
+    const editor = this.ref.current;
+    if (!editor) {
+      return;
+    }
+
+    const { predicate_object_maps, predicate_map, object_map } = names;
+    const workspace = editor.mainWorkspace;
+    createBlock(workspace, {
+      type: predicate_object_maps.name,
+      statements: {
+        [predicate_object_maps.predMapsStat]: {
+          type: predicate_map.name,
+        },
+        [predicate_object_maps.ObjMapsStat]: {
+          type: object_map.name,
+          fields: {
+            [object_map.typeDrop]: 'reference',
+            [object_map.mapValue]: this.props.newBlockParam.reference,
+          },
+        },
+      },
+    });
+  }
+
   private blocklyOptions: BlocklyOptions = {
     comments: true,
     collapse: true,
@@ -46,6 +81,7 @@ class BlocklyEditor extends Component<BlocklyEditorProps> {
   render() {
     return (
       <BlocklyContainer
+        ref={this.ref}
         blocklyOptions={this.blocklyOptions}
         customBlocks={rmlBlocks}
         customMenuOptions={customMenuOptions}
@@ -57,9 +93,14 @@ class BlocklyEditor extends Component<BlocklyEditorProps> {
   }
 }
 
-const blocklyEditorWrapper = connect(null, {
-  setMappingCode,
-});
+const blocklyEditorWrapper = connect(
+  store => ({
+    newBlockParam: store.editorCommand,
+  }),
+  {
+    setMappingCode,
+  }
+);
 
 type BlocklyEditorProps = ConnectedProps<typeof blocklyEditorWrapper>;
 

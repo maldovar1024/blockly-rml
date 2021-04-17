d5e4366b2041aa09ea9bcf7c82db56ec1e2df252
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-04-17 19:09:39 +0800

    feat: 添加展示映射结果的表格

--- src/result-manager/mapping-result.tsx
+++ src/result-manager/mapping-result.tsx
@@ -1,23 +1,65 @@
 /** 展示生成的映射代码的组件 */
 import { useAppDispatch, useAppSelector } from '@/stores';
 import { fetchMappingResult } from '@/stores/results';
-import { FailedMappingResult, MappingResultStatus } from '@/stores/types';
+import { MappingResultStatus } from '@/stores/types';
 import { CaretRightOutlined, LoadingOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Table } from 'antd';
+import { Quad } from 'n3';
 import { FC, useCallback } from 'react';
+
+interface RDFViewerProps {
+  quads: Quad[];
+}
+
+interface RDFTripleData {
+  key: number;
+  subject: string;
+  predicate: string;
+  object: string;
+}
+
+const { Column } = Table;
+
+const RDFViewer: FC<RDFViewerProps> = ({ quads }) => {
+  const data = quads.map<RDFTripleData>((quad, idx) => ({
+    key: idx,
+    subject: quad.subject.value,
+    predicate: quad.predicate.value,
+    object: quad.object.value,
+  }));
+
+  return (
+    <Table<RDFTripleData>
+      dataSource={data}
+      pagination={false}
+      size="small"
+      sticky
+      bordered
+    >
+      <Column title="Subject" dataIndex="subject" key="subject" />
+      <Column title="Predicate" dataIndex="predicate" key="predicate" />
+      <Column title="Object" dataIndex="object" key="object" />
+    </Table>
+  );
+};
 
 const MappingResult: FC = () => {
   const mappingResult = useAppSelector(store => store.results.mappingResult);
 
   let content;
-  if (mappingResult.status === MappingResultStatus.initial) {
-    content = '点击 ▶ 执行映射';
-  } else if (mappingResult.status === MappingResultStatus.pending) {
-    content = <LoadingOutlined />;
-  } else if (mappingResult.status === MappingResultStatus.successful) {
-    content = mappingResult.result;
-  } else {
-    content = (mappingResult as FailedMappingResult).message;
+  switch (mappingResult.status) {
+    case MappingResultStatus.initial:
+      content = '点击 ▶ 执行映射';
+      break;
+    case MappingResultStatus.pending:
+      content = <LoadingOutlined />;
+      break;
+    case MappingResultStatus.successful:
+      content = <RDFViewer quads={mappingResult.result} />;
+      break;
+    default:
+      content = mappingResult.message;
+      break;
   }
 
   const { status } = mappingResult;

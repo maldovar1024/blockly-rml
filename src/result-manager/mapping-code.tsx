1c4791fa71c5d7853b003f9a2dd2542ef4fc9d9f
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-04-20 23:04:06 +0800

    feat: 添加下载映射代码的功能

--- src/result-manager/mapping-code.tsx
+++ src/result-manager/mapping-code.tsx
@@ -1,13 +1,24 @@
 /** 展示生成的映射代码的组件 */
 import { useAppSelector } from '@/stores';
+import { downloadFile } from '@/utils';
+import { Button } from 'antd';
 import { FC } from 'react';
 
 const MappingCode: FC = () => {
   const code = useAppSelector(store => store.results.code);
+  const handleClick = () => {
+    downloadFile(code, 'rml-mapping.ttl');
+  };
+
   return (
-    <pre className="mapping-code">
-      <code>{code}</code>
-    </pre>
+    <>
+      <div className="controller">
+        <Button onClick={handleClick}>导出</Button>
+      </div>
+      <pre className="mapping-code">
+        <code>{code}</code>
+      </pre>
+    </>
   );
 };
 

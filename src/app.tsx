c4bd80def445dd047b26b5c9e77f3e2b36c5ad91
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-05-07 23:22:54 +0800

    feat: SourceManager 可收起

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,22 @@
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 import { FC } from 'react';
+import { useToggle } from 'react-use';
 import './app.less';
 import BlocklyEditor from './blockly-editor';
 import MappingResult from './result-manager';
 import SourceManager from './source-manager';
 
 const App: FC = () => {
+  const [collapsed, toggle] = useToggle(false);
+
   return (
-    <main className="app">
+    <main className={`app${collapsed ? ' collapsed' : ''}`}>
+      <Button
+        className="source-controller"
+        onClick={toggle}
+        icon={<ArrowLeftOutlined />}
+      />
       <SourceManager />
       <BlocklyEditor />
       <MappingResult />
